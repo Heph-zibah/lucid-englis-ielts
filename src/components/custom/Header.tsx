ab1b37727923672e090e060d16e34146ab4ef828
oaDaramola <oluwatosinhephzibah@gmail.com>
2025-07-23 23:42:19 +0100

    made an adjustment

--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -8,7 +8,6 @@ const Header = () => {
       <div className='bg-[url("/images/img-1.png")] bg-cover bg-center h-screen relative w-full  bg-black/50 z-0'>
         <div className="absolute top-0 left-0 w-full h-full bg-black/50 z-0"></div>
 
-        {/* Glassmorphism navbar */}
         <div className="z-20 fixed top-0 left-0 right-0 shadow-lg">
           <div className="px-5 md:px-10 lg:px-0 py-5 lg:max-w-7xl mx-auto flex justify-between items-center">
             <div className="flex flex-col  text-white text-xl md:text-2xl font-bold uppercase cursor-pointer">
@@ -46,9 +45,10 @@ const Header = () => {
           <div className="flex flex-col lg:flex-row items-center lg:justify-between ">
             <div className=" text-center lg:text-start ">
               <h1 className="text-2xl font-bold md:text-7xl">
-                <span className="text-[#E30613]"> Master</span> the IELTS Exam with Confidence
+                <span className="text-[#E30613]"> Master</span> the IELTS Exam
+                with Confidence
               </h1>
-              <p className="mt-4 max-w-xl mx-auto text-lg">
+              <p className="mt-4 max-w-xl mx-auto lg:mx-0 text-lg">
                 Learn how to listen, read, speak, and write like a pro — all
                 from the comfort of your home.
               </p>
